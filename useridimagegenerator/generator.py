be7aa7922f55792d5fcf2b3b087fc411b8f256c9
Andreas Pieger <andreas.pieger@epg.com>
2021-10-29 14:12:14 +0200

    Use name from URL and first rectangle tryout

--- useridimagegenerator/generator.py
+++ useridimagegenerator/generator.py
@@ -10,12 +10,16 @@ bp = Blueprint('generator', __name__, url_prefix='/generator')
 
 @bp.route('/<userid>', methods=['GET'])
 def image_generator(userid):
-    w = 100 # 100 pixels wide
-    h = 100 # 100 pixels high
-    img = Image.new('RGB', (w, h), color='#FF0000')
+    w = 200 # pixels width
+    h = 200 # pixels height
+    img = Image.new('RGBA', (w, h), color='#247963')
+    canvas = ImageDraw.Draw(img)
+    font = ImageFont.truetype('arial.ttf', size=24)
+    canvas.text((100, 100), escape(userid.upper()), anchor="ms", font=font, fill='#FFFFFF')
+    canvas.rectangle([(50, 150), (80,180)], fill="#EE8324")
 
     img_io = io.BytesIO()
-    img.save(img_io, 'JPEG', quality=70)
+    img.save(img_io, 'PNG', quality=70)
     img_io.seek(0)
 
-    return send_file(img_io, mimetype='image/jpeg')
+    return send_file(img_io, mimetype='image/png')
